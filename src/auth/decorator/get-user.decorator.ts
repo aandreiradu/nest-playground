a6126bca88aba6955a876409326630743ce2c79f
Andrei Radu <raduandrei697@gmail.com>
2023-05-05 00:31:48 +0300

    guard and custom decorator

---
+++ src/auth/decorator/get-user.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+
+    if (data) {
+      console.log('data', data);
+      return request.user[data];
+    }
+
+    return request.user;
+  },
+);
